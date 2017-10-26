e23f0dea63e3de7c61e874fd2f98937856f520c8
Quinn <quinn@twelve5designs.com>
2017-10-26 10:34:52 -0400

    Pushing curent

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
+//create new comment 
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './Components/search_bar';
+import VideoList from './Components/video_list';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyA1VHSxcYEk7VKe5c2uKh9KOGMaSApdwno';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+class App extends Component {
+
+    constructor(props)
+    {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>);
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
